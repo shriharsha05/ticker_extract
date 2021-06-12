77951f7f4c7bdd7a8fdfb93f70af0290901bebce
Shriharsha M <shriharsha05@gmail.com>
2021-06-12 22:54:23 +0530

    Version 1.0

--- ticker_extracter.py
+++ ticker_extracter.py
@@ -1,28 +1,10 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
-import re
+import csv
+import re		
 
-driver = webdriver.Firefox()					
-
-def read_nse_companies():
-    df = pd.read_excel('NSE_MAR21.xlsx',index_col=0)
-    # print(df)
-    # print(df['Symbol'])
-    for company in df['Symbol']:
-        print("\n", company)
-        get_webpage(company)
-
-def get_webpage(company):
-    try:
-        url = "https://ticker.finology.in/company/"+company+"/"
-        driver.get(url)
-        result = driver.page_source
-        extract_ratings(result)
-    except:
-        pass
-
-def extract_ratings(result):
+def extract_ratings(symbol, company_name, result):
     soup = BeautifulSoup(result,'html.parser')
     # print(soup.prettify())
     try:
@@ -31,15 +13,33 @@ def extract_ratings(result):
         valuation_rating = re.search(r'Valuation Rating is (.*?) out of 5', str(soup.find('div',id="mainContent_ValuationRating"))).group(1)
         efficiency_rating = re.search(r'Valuation Rating is (.*?) out of 5', str(soup.find('div',id="mainContent_EfficiencyRating"))).group(1)
         financials_rating = re.search(r'Valuation Rating is (.*?) out of 5', str(soup.find('div',id="mainContent_FinancialsRating"))).group(1)
-        print(overall_rating)
-        print(management_rating)
-        print(valuation_rating)
-        print(efficiency_rating)
-        print(financials_rating)
+        print(symbol, company_name, overall_rating, management_rating, valuation_rating, efficiency_rating, financials_rating)
+        csv_writer.writerow([symbol, company_name, overall_rating, management_rating, valuation_rating, efficiency_rating, financials_rating])
     except:
         pass
 
+def get_webpage(symbol, company_name):
+    url = "https://ticker.finology.in/company/"+symbol+"/"
+    try:
+        driver.get(url)
+    except:
+        print("Unable to load: ", url)
+        pass
+    result = driver.page_source
+    extract_ratings(symbol, company_name, result)
+
+def read_nse_companies():
+    df = pd.read_excel('NSE_MAR21.xlsx',index_col=0)
+    # print(df)
+    # print(df['Symbol'])
+    for symbol, company_name in zip(df['Symbol'],df['Company Name']):
+        get_webpage(symbol, company_name)
+
 if __name__ == "__main__":
-    driver = webdriver.Firefox()	
+    driver = webdriver.Firefox()
+    csv_file = open('nse_companies_valuations.csv', 'w')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Symbol', 'Company Name', 'Overall Rating','Ownership','Valuation','Efficiency','Financials'])
     read_nse_companies()
     driver.close()
+    csv_file.close()
